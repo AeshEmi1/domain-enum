0e0edf902daf397e35292bb001cf2dcae53f96af
AeshEmi1 <121838259+AeshEmi1@users.noreply.github.com>
2023-01-03 00:51:28 -0600

    Create subscan.py

---
+++ subscan.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import os
+
+# Parse the command-line arguments
+parser = argparse.ArgumentParser(description="Scan a list of domains for subdomains using Amass and Subfinder")
+parser.add_argument("input_file", help="the file containing the list of domains to scan")
+parser.add_argument("output_file", help="the file to write the unique subdomains to")
+parser.add_argument("-k", "--keep", action="store_true", help="Keeps the Amass and Subfinder output files separately")
+parser.add_argument("-v", "--verbose", action="store_true", help="Show the output of the Amass and Subfinder commands in real time")
+args = parser.parse_args()
+
+amass_file = "{}-amass.txt".format(args.output_file)
+subfinder_file = "{}-subfinder.txt".format(args.output_file)
+output_file = "{}.txt".format(args.output_file)
+
+# Open the output file for writing
+with open(output_file, "w") as f:
+    # Iterate over the list of domains
+    amass_command = ["amass", "enum", "-brute", "-df",  args.input_file, "-o", amass_file]
+    amass_command_verbose = ["amass", "enum", "-brute", "-df",  args.input_file, "-o", amass_file, "-v"]
+    if args.verbose:
+        result = subprocess.run(amass_command_verbose)
+    else:
+        subprocess.run(amass_command)
+
+    subfinder_command = ["subfinder", "-dL", args.input_file, "-all", "-o", subfinder_file, "-silent"]
+    subfinder_command_verbose = ["subfinder", "-dL", args.input_file, "-all", "-o", subfinder_file]
+    if args.verbose:
+        result = subprocess.run(subfinder_command_verbose)
+    else:
+        subprocess.run(subfinder_command)
+
+    # Combine the amass and subfinder output
+    with open(amass_file, "r") as am, open(subfinder_file, "r") as su, open(output_file, "w") as out:
+        out.write(am.read())
+        out.write(su.read())
+
+    # Remove duplicate subdomains
+    with open(output_file, "r") as f:
+        lines = f.readlines()
+
+    lines_set = set(lines)
+
+    # Write the unique subdomains to the output file
+    with open(output_file, "w") as f:
+        for line in lines_set:
+            f.write(line)
+
+if os.path.exists(amass_file) and not args.keep:
+    os.remove(amass_file)
+
+if os.path.exists(subfinder_file) and not args.keep:
+    os.remove(subfinder_file)

092b1353fd806d58bab91f579b82a7f9a7746bb1
AeshEmi1 <121838259+AeshEmi1@users.noreply.github.com>
2023-01-03 10:30:54 -0600

    Update subscan.py
    Performance improvements (added -dns-qps 3000). Added support for logging. Added configuration file support.

--- subscan.py
+++ subscan.py
@@ -10,6 +10,7 @@ parser.add_argument("input_file", help="the file containing the list of domains 
 parser.add_argument("output_file", help="the file to write the unique subdomains to")
 parser.add_argument("-k", "--keep", action="store_true", help="Keeps the Amass and Subfinder output files separately")
 parser.add_argument("-v", "--verbose", action="store_true", help="Show the output of the Amass and Subfinder commands in real time")
+parser.add_argument("-l", "--logging", action="store_true", help="Enable logging for amass")
 args = parser.parse_args()
 
 amass_file = "{}-amass.txt".format(args.output_file)
@@ -18,36 +19,40 @@ output_file = "{}.txt".format(args.output_file)
 
 # Open the output file for writing
 with open(output_file, "w") as f:
-    # Iterate over the list of domains
-    amass_command = ["amass", "enum", "-brute", "-df",  args.input_file, "-o", amass_file]
-    amass_command_verbose = ["amass", "enum", "-brute", "-df",  args.input_file, "-o", amass_file, "-v"]
-    if args.verbose:
-        result = subprocess.run(amass_command_verbose)
-    else:
-        subprocess.run(amass_command)
+	# Iterate over the list of domains
+	amass_command = ["amass", "enum", "-dns-qps", "3000", "-brute", "-df",  args.input_file, "-o", amass_file, "-config", "~/.config/amass/config.ini"]
+	if args.verbose:
+		amass_command.append("-v")
+		amass_command.append("-src")
+	if args.logging:
+		amass_command.append("-log")
+		amass_command.append("{}.log".format(amass_file))
 
-    subfinder_command = ["subfinder", "-dL", args.input_file, "-all", "-o", subfinder_file, "-silent"]
-    subfinder_command_verbose = ["subfinder", "-dL", args.input_file, "-all", "-o", subfinder_file]
-    if args.verbose:
-        result = subprocess.run(subfinder_command_verbose)
-    else:
-        subprocess.run(subfinder_command)
+	subprocess.run(amass_command)
+
+	subfinder_command = ["subfinder", "-dL", args.input_file, "-all", "-o", subfinder_file, "-silent"]
+	subfinder_command_verbose = ["subfinder", "-dL", args.input_file, "-all", "-o", subfinder_file]
+
+	if args.verbose:
+		subprocess.run(subfinder_command_verbose)
+	else:
+		subprocess.run(subfinder_command)
 
     # Combine the amass and subfinder output
-    with open(amass_file, "r") as am, open(subfinder_file, "r") as su, open(output_file, "w") as out:
-        out.write(am.read())
-        out.write(su.read())
+	with open(amass_file, "r") as am, open(subfinder_file, "r") as su, open(output_file, "w") as out:
+		out.write(am.read())
+		out.write(su.read())
 
     # Remove duplicate subdomains
-    with open(output_file, "r") as f:
-        lines = f.readlines()
+	with open(output_file, "r") as f:
+		lines = f.readlines()
 
-    lines_set = set(lines)
+	lines_set = set(lines)
 
     # Write the unique subdomains to the output file
-    with open(output_file, "w") as f:
-        for line in lines_set:
-            f.write(line)
+	with open(output_file, "w") as f:
+		for line in lines_set:
+			f.write(line)
 
 if os.path.exists(amass_file) and not args.keep:
     os.remove(amass_file)

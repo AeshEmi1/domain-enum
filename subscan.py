89d5d56760288fdc49571f78420ab61a57a34cb9
AeshEmi1 <121838259+AeshEmi1@users.noreply.github.com>
2023-01-03 18:22:53 -0600

    Update subscan.py
    Fixed verbosity (output no longer displays the source). Added wordlist argument support for Amass. Sorts the output file by root domain.

--- subscan.py
+++ subscan.py
@@ -8,6 +8,7 @@ import os
 parser = argparse.ArgumentParser(description="Scan a list of domains for subdomains using Amass and Subfinder")
 parser.add_argument("input_file", help="the file containing the list of domains to scan")
 parser.add_argument("output_file", help="the file to write the unique subdomains to")
+parser.add_argument("-w", "--wordlist", help="Specify a wordlist for brute forcing", default=None)
 parser.add_argument("-k", "--keep", action="store_true", help="Keeps the Amass and Subfinder output files separately")
 parser.add_argument("-v", "--verbose", action="store_true", help="Show the output of the Amass and Subfinder commands in real time")
 parser.add_argument("-l", "--logging", action="store_true", help="Enable logging for amass")
@@ -20,13 +21,16 @@ output_file = "{}.txt".format(args.output_file)
 # Open the output file for writing
 with open(output_file, "w") as f:
 	# Iterate over the list of domains
-	amass_command = ["amass", "enum", "-dns-qps", "3000", "-brute", "-df",  args.input_file, "-o", amass_file, "-config", os.path.expanduser("~/.config/amass/config.ini")]
+	amass_command = ["amass", "enum", "-brute", "-df",  args.input_file, "-o", amass_file, "-config", os.path.expanduser("~/.config/amass/config.ini")]
 	if args.verbose:
 		amass_command.append("-v")
-		amass_command.append("-src")
 	if args.logging:
 		amass_command.append("-log")
 		amass_command.append("{}.log".format(amass_file))
+	if args.wordlist is not None:
+		amass_command.append("-w")
+		amass_command.append(args.wordlist)
+
 
 	subprocess.run(amass_command)
 
@@ -48,10 +52,20 @@ with open(output_file, "w") as f:
 		lines = f.readlines()
 
 	lines_set = set(lines)
+	
+	# Extract the root domain from each subdomain
+	root_domains = []
+	for subdomain in lines_set:
+		domain = urlparse(subdomain).netloc
+		root_domain = ".".join(domain.split(".")[-2:])
+		root_domains.append(root_domain)
 
-    # Write the unique subdomains to the output file
+	# Zip the subdomains and root domains together and sort them by root domain
+	sorted_subdomains = [subdomain for _, subdomain in sorted(zip(root_domains, lines_set))]
+
+	# Write the sorted subdomains to the output file
 	with open(output_file, "w") as f:
-		for line in lines_set:
+		for line in sorted_subdomains:
 			f.write(line)
 
 if os.path.exists(amass_file) and not args.keep:
